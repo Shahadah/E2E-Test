75cbd04ae026e79d0def0428e20f669d1671b64f
n.ragimi <68182523+n.ragimi@users.noreply.gitlab.loc>
2024-10-17 11:32:35 +0800

    New test case and test suite for Payments

---
+++ Test Suites/Payments Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Payments Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>cefb1492-a0ef-4c4e-9a6d-bfee55a77bb8</testSuiteGuid>
+</TestSuiteEntity>
